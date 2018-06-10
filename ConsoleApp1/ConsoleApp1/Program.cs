cde3219472e04da0531ff36b91565c9f634864d7
timur97perm <timbakagir@gmail.com>
2018-06-10 20:21:54 +0500

    Программа готова

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0;
+            double x=0;
+            bool succ = false;
+
+            do
+            {   Console.WriteLine("Введите целое число n");
+                succ = int.TryParse(Console.ReadLine(), out n);
+                if (!succ) Console.WriteLine("Введите целое число");
+            } while (!succ);
+
+            do
+            {
+                Console.WriteLine("Введите число x");
+                succ = double.TryParse(Console.ReadLine(), out x);
+                if (!succ) Console.WriteLine("Введите число");
+            } while (!succ);
+
+            double[,] mas = new double[n, 2 * n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < (n*2); j++)
+                {
+                    mas[i, j] = random.Next(100);
+                    Console.Write(mas[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            bool b = true;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < (n * 2); j++)
+                {
+                    if (mas[i, j] > x)
+                        b = false;
+                }
+                if (b == true)
+                    Console.Write(1 + ", ");
+                else Console.Write(0 + ", ");
+                b = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
